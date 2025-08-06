7891e62efb53ac3dc02129be457eb3727e7a3d39
“Riaz <riazahmed3919@gmail.com>
2025-08-06 20:24:01 +0600

    Render db reseted

--- event_management/settings.py
+++ event_management/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://event_manager_db_unwt_user:O1a9LMHni7LRDKXOPp1PZ5jFNRKQgKKs@dpg-d1iji6adbo4c73f9ne20-a.oregon-postgres.render.com/event_manager_db_unwt',
+        default='postgresql://event_management_db_4g4o_user:O9h3r0d1bvn6C7cdrgJZtdm4V6JVSoxT@dpg-d29m9mbe5dus73cn7cig-a.oregon-postgres.render.com/event_management_db_4g4o',
         conn_max_age=600
     )
 }
